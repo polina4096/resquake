bfe16c354dfe6e4594aa67c24c8b09eed5d690e5
polina4096 <polina4096@yandex.ru>
2025-01-02 00:37:31 +0300

    Document code that cancels fall damage velocity and fix unused warnings

--- src/main/java/polina4096/resquake/mixin/MixinPlayerEntity.java
+++ src/main/java/polina4096/resquake/mixin/MixinPlayerEntity.java
@@ -14,45 +14,46 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import polina4096.resquake.ReSquakePlayer;
 
+@SuppressWarnings("unused")
 @Mixin(PlayerEntity.class)
 public abstract class MixinPlayerEntity extends LivingEntity {
-	protected MixinPlayerEntity(EntityType<? extends LivingEntity> entityType, World world) {
-		super(entityType, world);
-	}
+  protected MixinPlayerEntity(EntityType<? extends LivingEntity> entityType, World world) {
+    super(entityType, world);
+  }
 
-	@Inject(method = "jump", at = @At("TAIL"))
-	public void jumpInject(CallbackInfo ci) {
-		PlayerEntity player = (PlayerEntity)(Object)this;
-		ReSquakePlayer.INSTANCE.afterJump(player);
-	}
+  @Inject(method = "jump", at = @At("TAIL"))
+  public void jumpInject(CallbackInfo ci) {
+    PlayerEntity player = (PlayerEntity) (Object) this;
+    ReSquakePlayer.INSTANCE.afterJump(player);
+  }
 
-	@Inject(method = "travel", at = @At("HEAD"), cancellable = true)
-	public void travelInject(Vec3d movementInput, CallbackInfo ci) {
-		PlayerEntity player = (PlayerEntity)(Object)this;
-		if (ReSquakePlayer.INSTANCE.travel(player, movementInput)) {
-			ci.cancel();
-		}
-	}
+  @Inject(method = "travel", at = @At("HEAD"), cancellable = true)
+  public void travelInject(Vec3d movementInput, CallbackInfo ci) {
+    PlayerEntity player = (PlayerEntity) (Object) this;
+    if (ReSquakePlayer.INSTANCE.travel(player, movementInput)) {
+      ci.cancel();
+    }
+  }
 
-	@Inject(method = "tick", at = @At("HEAD"))
-	public void tickInject(CallbackInfo ci) {
-		PlayerEntity player = (PlayerEntity)(Object)this;
-		ReSquakePlayer.INSTANCE.beforeTick(player);
-	}
+  @Inject(method = "tick", at = @At("HEAD"))
+  public void tickInject(CallbackInfo ci) {
+    PlayerEntity player = (PlayerEntity) (Object) this;
+    ReSquakePlayer.INSTANCE.beforeTick(player);
+  }
 
-	public boolean velocityChanged = false;
+  public boolean velocityChanged = false;
 
-	@Inject(at = @At("HEAD"), method = "handleFallDamage")
-	private void beforeFall(float fallDistance, float damageMultiplier, DamageSource damageSource, CallbackInfoReturnable<Boolean> cir)
-	{
-		if (!this.getWorld().isClient)
-			velocityChanged = this.velocityModified;
-	}
+  @Inject(at = @At("HEAD"), method = "handleFallDamage")
+  private void beforeFall(float fallDistance, float damageMultiplier, DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
+    // Store velocity changed state before fall damage is applied
+    if (!this.getWorld().isClient)
+      velocityChanged = this.velocityModified;
+  }
 
-	@Inject(method = "handleFallDamage", at = @At("RETURN"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;increaseStat(Lnet/minecraft/util/Identifier;I)V"), to = @At("TAIL")))
-	private void afterFall(float fallDistance, float damageMultiplier, DamageSource damageSource, CallbackInfoReturnable<Boolean> cir)
-	{
-		if (!this.getWorld().isClient)
-			this.velocityModified = velocityChanged;
-	}
+  @Inject(method = "handleFallDamage", at = @At("RETURN"), slice = @Slice(from = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;increaseStat(Lnet/minecraft/util/Identifier;I)V"), to = @At("TAIL")))
+  private void afterFall(float fallDistance, float damageMultiplier, DamageSource damageSource, CallbackInfoReturnable<Boolean> cir) {
+    // Cancel fall damage velocity by restoring state to original one before fall damage was applied if the player is bunnyhopping
+    if (!this.getWorld().isClient)
+      this.velocityModified = velocityChanged;
+  }
 }
